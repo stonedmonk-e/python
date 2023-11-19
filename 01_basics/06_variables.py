ec31ff861e758723b073f73a746c782b2ec533d2
Shashank S <111871083+stonedmonk-e@users.noreply.github.com>
2023-11-19 07:40:12 +0530

    Update 06_variables.py, global variables

--- 01_basics/06_variables.py
+++ 01_basics/06_variables.py
@@ -1,13 +1,15 @@
 print("Hello, World!")
 
-# The number of spaces is up to you as a programmer, the most common use is four, but it has to be at least one
+# The number of spaces is up to you as a programmer, 
+# the most common use is four, but it has to be at least one
 
 if 5 > 2:
  print("Five is greater than two!") 
 if 5 > 2:
         print("Five is greater than two!") 
 
-# You have to use the same number of spaces in the same block of code, otherwise Python will give you an error:
+# You have to use the same number of spaces in the same block of code, 
+# otherwise Python will give you an error:
 # Syntax Error:
 
 """ => this is multiline string, another way to comment
@@ -79,10 +81,44 @@ print(x)
 print(y)
 print(z)
 
-# If you have a collection of values in a list, tuple etc. Python allows you to extract the values into variables. This is called unpacking.
+# If you have a collection of values in a list, tuple etc. 
+# Python allows you to extract the values into variables. This is called unpacking.
 # Unpack a list:
 fruits = ["apple", "banana", "cherry"]
 x, y, z = fruits   # make sure you're extracting all of them
 print(x)
 print(y)
 print(z)
+
+
+# Output variables
+# print can be used to output multiple variables
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+print(x+y+z)
+
+#different data types
+a = 5
+# print(a+z) => cannot be done, so use comma
+print(a, z); #5 awesome
+
+
+# Global Variables
+
+x = "awesome"
+
+def myfunc():
+    x = "fantastic"
+    print("python is "+x) # python is fantastic
+
+myfunc()
+
+print("Python is "+x) # Python is awesome
+
+# To change the value of a global variable inside a function,
+# refer to the variable by using the global keyword:
+def myfunc():
+  global x
+  x = "fantastic"
